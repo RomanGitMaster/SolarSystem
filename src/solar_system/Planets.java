4d6d5d52d12fe7bff8cbac0c19155aec835f4d0e
RomanGitMaster <roman.yasinovskyy29@gmail.com>
2026-03-24 12:28:42 -0400

    solar system structure is ready

---
+++ src/solar_system/Planets.java
@@ -0,0 +1,19 @@
+package solar_system;
+
+public class Planets {
+    public static void main(String[] args) {
+
+
+        String planet1 = "Mercury";
+        String planet2 = "Venus";
+        String planet3 = "Earth";
+        String planet4 = "Jupitor";
+        String planet5 = "Saturn";
+        String planet6 = "Uranus";
+        String planet7 = "Neptune";
+        String planet8 = "Saturn";
+
+        int planetCount = 8;
+        System.out.println("Solar system has " + planetCount + " planets, and they are: " + planet1 + " " + planet2 + " " + planet3 + " " + planet4 + " " + planet5 + " " + planet6 + " " + planet7 + " " + planet8);
+    }
+}
---
+++ src/solar_system/Star.java
@@ -0,0 +1,9 @@
+package solar_system;
+
+public class Star {
+    static String star = "SUN";
+
+    public static void main(String[] args) {
+        System.out.println("Solar system star is " + star);
+    }
+}
